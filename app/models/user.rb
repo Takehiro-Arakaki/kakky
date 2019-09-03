211726f12bd605b4c82b174fb82964b58367ea36
take1007 <takehiro1308@yahoo.co.jp>
2019-09-03 20:49:22 +0900

    delete admin and make new admin migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+
+  # NOTE:これだけでデータベースのカラムには対応しない二つの属性
+  # (password,password_confirmation)属性が生成される
+  has_secure_password
+
+  #Scopes
+  default_scope { order(:id) }
+
+  #Validations
+  # validates  :name, :email, :password_digest
+
 end
