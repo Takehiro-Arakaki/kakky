39c4571bc521cda5ff08e58a9c4aade7c0cd8737
take1007 <takehiro1308@yahoo.co.jp>
2019-11-02 12:45:18 +0900

    fix slim

--- db/migrate/20190811023822_create_questions.rb
+++ db/migrate/20190811023822_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[5.2]
   def change
     create_table :questions, id: :integer do |t|
       t.integer :level_id,         null: false
-      t.string :title
+      t.string :title,             default: ''
       t.string :question_num,      null: false
       t.text :content,             null: false, default: ''
 
